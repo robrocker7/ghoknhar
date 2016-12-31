2ceb728af345e240d8f9129aa490655f23d31c65
Robert Johnson <robrocker7@gmail.com>
2016-12-31 14:38:01 -0600

    added test reponse

--- website/actions/views.py
+++ website/actions/views.py
@@ -1,3 +1,4 @@
+import json
 from hashlib import sha1
 
 from rest_framework import status, viewsets
@@ -16,17 +17,28 @@ class ActionsViewSet(viewsets.GenericViewSet):
 
     @list_route(methods=['POST'])
     def actions(self, request):
-        print request.data
         try:
             result = GoogleActionResultSerializer(request.data)
         except Exception as e:
             print str(e)
-        result = request.data['result']
-        params = result['parameters']
-        action = result['action']
-        id = request.data['id']
-        status = request.data['status']
+            return Response({
+                'success': True
+            })
 
-        return Response({
-            'success': True
+        response = GoogleActionResponseSerializer()
+        response.id = result.id
+        response.timestamp = result.timestamp
+        response.status = result.status
+        response.sessionId = result.sessionId
+        response_result = result.result
+        response_result.fulfillment = json.dumps({
+            'speech': 'I will add that day to your Work Schedule',
+            'messages': [{
+                'type': 0,
+                'speech': 'I will add that day to your Work Schedule'
+            }]
         })
+        response.result = response_result
+        print response.data
+        print json.dumps(response.data)
+        return Response(response.data)
