029750a35bbe308447ab9126f7c6945a1e6c1f9c
Robert Johnson <robrocker7@gmail.com>
2016-12-31 14:27:20 -0600

    added serializers

--- website/actions/serializers.py
+++ website/actions/serializers.py
@@ -1,46 +1,31 @@
 from rest_framework import serializers
 
-class GoogleActionSerializer(serializers.Serializer):
-    """
-    {
-  "id": "b7cfa4ac-8bcc-444e-918b-405967efd833",
-  "timestamp": "2016-12-31T00:27:19.348Z",
-  "result": {
-    "source": "agent",
-    "resolvedQuery": "Add December 3rd to my Work Schedule",
-    "action": "calendar.add",
-    "actionIncomplete": false,
-    "parameters": {
-      "date-time": "December 3rd ",
-      "workschedule": "workschedule.action"
-    },
-    "contexts": [],
-    "metadata": {
-      "intentId": "9cb46c0d-1603-4ffd-8ed1-aaa737ed2c76",
-      "webhookUsed": "false",
-      "webhookForSlotFillingUsed": "false",
-      "intentName": "Add Work Day to Schedule"
-    },
-    "fulfillment": {
-      "speech": "Alright. I've added that to your work schedule.",
-      "messages": [
-        {
-          "type": 0,
-          "speech": "Alright. I've added that to your work schedule."
-        }
-      ]
-    },
-    "score": 1
-  },
-  "status": {
-    "code": 200,
-    "errorType": "success"
-  },
-  "sessionId": "0e67f6c9-ddd6-4e9b-a7e4-90a2b2a55e2d"
-}
-    """
-    id = serializers.UUIDField()
-    session_id = serializers.UUIDField()
-    timestamp = serializers.DateTimeField()
+class GoogleActionStatusSerializer(serializers.Serializer):
+    code = serializers.IntegerField()
+    errorType = serializers.CharField()
+
+class GoogleActionResultSerializer(serializers.Serializer):
+    source = serializers.CharField()
+    resolvedQuery = serializers.CharField()
+    action = serializers.CharField()
+    actionIncomplete = serializers.CharField()
     parameters = serializers.CharField()
-    status_code = serializers.CharField()
+    contexts = serializers.CharField()
+    fulfillment = serializers.CharField()
+    score = serializers.CharField()
+    metadata = serializers.CharField()
+
+class GoogleActionResponseSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    timestamp = serializers.CharField()
+    status = GoogleActionStatusSerializer()
+    sessionId = serializers.UUIDField()
+    result = GoogleActionResultSerializer()
+
+
+class GoogleActionResultSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    timestamp = serializers.CharField()
+    status = GoogleActionStatusSerializer()
+    sessionId = serializers.UUIDField()
+    result = GoogleActionResultSerializer()
--- website/actions/views.py
+++ website/actions/views.py
@@ -7,16 +7,26 @@ from rest_framework.response import Response
 from rest_framework.decorators import list_route, detail_route
 from django.template.loader import get_template
 
-from .serializers import GoogleActionSerializer
+from .serializers import GoogleActionResponseSerializer, GoogleActionResultSerializer
 
 class ActionsViewSet(viewsets.GenericViewSet):
-    serializer_class = GoogleActionSerializer
+    serializer_class = GoogleActionResultSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     @list_route(methods=['POST'])
     def actions(self, request):
         print request.data
+        try:
+            result = GoogleActionResultSerializer(request.data)
+        except Exception as e:
+            print str(e)
+        result = request.data['result']
+        params = result['parameters']
+        action = result['action']
+        id = request.data['id']
+        status = request.data['status']
+
         return Response({
             'success': True
         })
