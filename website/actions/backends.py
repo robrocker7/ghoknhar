a2ed6c2a833d9a5623cec1c3c9f40084cdd7232b
Robert Johnson <robrocker7@gmail.com>
2017-01-19 15:12:48 -0600

    added more backend functions

--- website/actions/backends.py
+++ website/actions/backends.py
@@ -2,4 +2,20 @@ from social_core.backends.google import GooglePlusAuth
 
 
 class GooglePlusActionAuth(GooglePlusAuth):
-	name = 'google-plus-action'
+    name = 'google-plus-action'
+    DEFAULT_SCOPE = [
+        'https://www.googleapis.com/auth/plus.login',
+        'https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/userinfo.profile'
+    ]
+
+    def user_data(self, access_token, *args, **kwaargs):
+        """Return user data from Google API"""
+        print access_token
+        return self.get_json(
+            'https://www.googleapis.com/plus/v1/people/me',
+            params={
+                'access_token': access_token,
+                'alt': 'json'
+            }
+        
